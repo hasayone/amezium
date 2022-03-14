a88b7fe264610200b477ee5967152358775fcdd4
Alexandr Orlovskiy <hasayone@gmail.com>
2022-03-14 16:59:40 +0200

    init

---
+++ index.php
@@ -0,0 +1,561 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!--
+		Basic
+	-->
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Dmirtiy Orlovskiy - Android Developer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="description" content="Dmirtiy Orlovskiy - Android Developer" />
+	<meta name="keywords" content="android, kotlin, portfolio" />
+	<meta name="author" content="empat.dev" />
+
+	<!--
+		Load Fonts
+	-->
+	<link
+		href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+		rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css">
+
+	<!--
+		Load CSS
+	-->
+	<link rel="stylesheet" href="css/basic.css" />
+	<link rel="stylesheet" href="css/layout.css" />
+	<link rel="stylesheet" href="css/ionicons.css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" />
+	<link rel="stylesheet" href="css/animate.css" />
+	<link rel="stylesheet" href="css/owl.carousel.css" />
+
+	<!--
+		Background Gradient
+	-->
+	<link rel="stylesheet" href="css/gradient.css" />
+	<link rel="stylesheet" href="css/new-skin/new-skin.css" />
+	<link rel="stylesheet" href="css/template-colors/green.css" />
+
+
+	<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!--
+		Favicons
+	-->
+	<link rel="shortcut icon" href="images/favicons/favicon.ico">
+
+</head>
+
+<body>
+	<div class="page">
+
+		<!-- preloader -->
+		<div class="preloader">
+			<div class="centrize full-width">
+				<div class="vertical-center">
+					<div class="spinner">
+						<div class="double-bounce1"></div>
+						<div class="double-bounce2"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- background -->
+		<div class="background gradient">
+			<ul class="bg-bubbles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
+
+		<!--
+			Container
+		-->
+		<div class="container opened" data-animation-in="fadeInLeft" data-animation-out="fadeOutLeft">
+
+			<!--
+				Header
+			-->
+			<header class="header">
+
+				<!-- menu -->
+				<div class="top-menu">
+					<ul>
+						<li class="active">
+							<a href="#about-card">
+								<span class="icon ion-person"></span>
+								<span class="link">About</span>
+							</a>
+						</li>
+						<li>
+							<a href="#resume-card">
+								<span class="icon ion-android-list"></span>
+								<span class="link">Resume</span>
+							</a>
+						</li>
+						<!-- <li>
+							<a href="#works-card">
+								<span class="icon ion-code"></span>
+								<span class="link">Works</span>
+							</a>
+						</li> -->
+						<li>
+							<a href="#contacts-card">
+								<span class="icon ion-at"></span>
+								<span class="link">Contact</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+
+			</header>
+
+			<!--
+				Card - Started
+			-->
+			<div class="card-started" id="home-card">
+
+				<!--
+					Profile
+				-->
+				<div class="profile">
+
+					<!-- profile image -->
+					<div class="slide" style="background-image: url(images/bg.jpg);"></div>
+
+					<!-- profile photo -->
+					<div class="image">
+						<img src="images/profile.png" alt="" />
+					</div>
+
+					<!-- profile titles -->
+					<div class="title">Dmitriy Orlovskiy</div>
+					<div class="subtitle">Android Developer</div>
+
+					<!-- profile socials -->
+					<div class="social">
+						<a target="_blank" href="https://t.me/amezium/"><span class="fab fa-telegram"></span></a>
+						<a target="_blank" href="https://linkedin.com/in/amezium/"><span class="fab fa-linkedin"></span></a>
+						<a target="_blank" href="https://djinni.co/q/a987e11557/"><span class="fab">
+								<svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px"
+									viewBox="0 0 112.000000 100.000000" preserveAspectRatio="xMidYMid meet">
+									<g transform="translate(0.000000,100.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+										<path
+											d="M673 981 c-82 -51 -45 -181 51 -181 42 0 91 39 99 77 7 40 -16 89 -52 107 -39 21 -59 20 -98 -3z" />
+										<path
+											d="M380 882 c-46 -23 -60 -53 -60 -134 0 -38 -2 -68 -5 -68 -3 0 -17 8 -31 17 -33 21 -110 21 -150 0 -37 -19 -77 -62 -100 -107 -23 -45 -31 -192 -14 -246 42 -133 161 -189 274 -129 26 14 49 25 50 25 2 0 6 -9 9 -20 5 -19 12 -21 74 -18 l68 3 3 348 2 347 -42 0 c-24 0 -59 -8 -78 -18z m-85 -314 c24 -11 25 -14 25 -113 0 -117 -6 -128 -67 -123 -54 4 -76 48 -70 138 4 54 9 68 32 87 30 26 43 28 80 11z" />
+										<path
+											d="M636 428 c-4 -150 -12 -288 -17 -305 -6 -18 -28 -48 -50 -67 -46 -39 -39 -46 45 -46 77 0 134 32 166 95 25 49 25 54 29 322 l3 273 -85 0 -84 0 -7 -272z" />
+										<path
+											d="M970 388 c-14 -7 -33 -30 -43 -50 -16 -33 -16 -40 -4 -71 38 -89 146 -90 183 -1 12 29 12 39 1 67 -8 17 -21 38 -30 45 -23 19 -79 24 -107 10z" />
+									</g>
+								</svg>
+							</span>
+						</a>
+						<a target="_blank" href="https://github.com/amezium"><span class="fab fa-github"></span></a>
+
+					</div>
+
+					<!-- profile buttons -->
+					<div class="lnks">
+						<a href="./Dmitriy-Orlovskiy-CV.pdf" target="_blank" class="lnk" download>
+							<span class="text">Download CV</span>
+							<span class="ion ion-archive"></span>
+						</a>
+						<a href="#" class="lnk discover">
+							<span class="text">Contact Me</span>
+							<span class="arrow"></span>
+						</a>
+					</div>
+
+				</div>
+
+			</div>
+
+			<!-- 
+				Card - About
+			-->
+			<div class="card-inner animated active" id="about-card">
+				<div class="card-wrap">
+
+					<!-- 
+						About 
+					-->
+					<div class="content about">
+
+						<!-- title -->
+						<div class="title">About Me</div>
+
+						<!-- content -->
+						<div class="row">
+							<div class="col col-d-12 col-t-12 col-m-12 border-line-v">
+								<div class="text-box">
+									<p>I'm Dmitriy Orlovskiy, an Android developer from Ukraine. Until 2021, he was engaged in
+										investments
+										and entrepreneurship in various fields of activity, more than 5 years. Investments remained in my
+										life, but the activities of the entrepreneur had to be stopped after a number of unsuccessful
+										projects. I am a full-time Android developer since 2021 and I really enjoy this phase of my life.
+									</p>
+									<br>
+									<p>
+										My main trait is perseverance. I chose programming for myself as a tool to achieve my goals and
+										enjoy
+										the development process itself.
+									</p>
+									<br>
+								</div>
+								<div class="info-list">
+									<ul>
+										<li><strong>Age . . . . .</strong> 27</li>
+										<li><strong>Residence . . . . .</strong> Ukraine</li>
+										<li><strong>Job Search / Freelance . . . . .</strong> Available</li>
+										<li><strong>I'm considering options . . . . .</strong> Remote Job</li>
+									</ul>
+								</div>
+							</div>
+							<div class="clear"></div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<!--
+				Card - Resume
+			-->
+			<div class="card-inner" id="resume-card">
+				<div class="card-wrap">
+
+					<!--
+								Skills
+							-->
+					<div class="content skills">
+
+						<!-- title -->
+						<div class="title">My Skills</div>
+
+						<!-- content -->
+						<div class="row">
+
+
+							<!-- skill item -->
+							<div class="col col-d-6 col-t-6 col-m-12 border-line-v">
+								<div class="skills-list circles">
+									<div class="skill-title border-line-h">
+										<div class="icon"><i class="ion ion-code"></i></div>
+										<div class="name">Coding</div>
+									</div>
+									<ul>
+										<li>
+											<div class="name">Kotlin</div>
+											<div class="progress p90">
+												<span>90%</span>
+											</div>
+										</li>
+										<li>
+											<div class="name">Firebase / SQL</div>
+											<div class="progress p75">
+												<span>75%</span>
+											</div>
+										</li>
+										<li>
+											<div class="name">Solid Principe</div>
+											<div class="progress p85">
+												<span>85%</span>
+											</div>
+										</li>
+										<li>
+											<div class="name">MVVM/MVP</div>
+											<div class="progress p95">
+												<span>95%</span>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+
+							<!-- skill item -->
+							<div class="col col-d-6 col-t-6 col-m-12 border-line-v">
+								<div class="skills-list dotted">
+									<div class="skill-title border-line-h">
+										<div class="icon"><i class="ion ion-flag"></i></div>
+										<div class="name">Languages</div>
+									</div>
+									<ul>
+										<li class="border-line-h">
+											<div class="name">Ukraine</div>
+											<div class="progress">
+												<div class="percentage" style="width:97.5%;"></div>
+											</div>
+										</li>
+										<li class="border-line-h">
+											<div class="name">Russian</div>
+											<div class="progress">
+												<div class="percentage" style="width:90%;"></div>
+											</div>
+										</li>
+										<li class="border-line-h">
+											<div class="name">English</div>
+											<div class="progress">
+												<div class="percentage" style="width:30%;"></div>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+
+							<!-- skill item -->
+							<div class="col col-d-12 col-t-12 col-m-12 border-line-v">
+								<div class="skills-list list">
+									<div class="skill-title border-line-h">
+										<div class="icon"><i class="ion ion-android-list"></i></div>
+										<div class="name">Soft Skills</div>
+									</div>
+									<ul>
+										<li>
+											<div class="name">Clean Architecture</div>
+										</li>
+										<li>
+											<div class="name">OOP</div>
+										</li>
+
+										<li>
+											<div class="name">Coroutines</div>
+										</li>
+										<li>
+											<div class="name">Android Jetpack</div>
+										</li>
+										<li>
+											<div class="name">Git / GitFlow</div>
+										</li>
+										<li>
+											<div class="name">Fragment</div>
+										</li>
+										<li>
+											<div class="name">Service</div>
+										</li>
+										<li>
+											<div class="name">RecyclerView</div>
+										</li>
+										<li>
+											<div class="name">ROOM</div>
+										</li>
+										<li>
+											<div class="name">Retrofit</div>
+										</li>
+										<li>
+											<div class="name">Rx Java 2</div>
+										</li>
+										<li>
+											<div class="name">Dagger 2</div>
+										</li>
+										<li>
+											<div class="name">SQLite</div>
+										</li>
+										<li>
+											<div class="name">Android SDK</div>
+										</li>
+										<li>
+											<div class="name">MVP</div>
+										</li>
+										<li>
+											<div class="name">AppsFlyer</div>
+										</li>
+										<li>
+											<div class="name">OneSignal</div>
+										</li>
+										<li>
+											<div class="name">API REST</div>
+										</li>
+										<li>
+											<div class="name">Figma</div>
+										</li>
+
+									</ul>
+								</div>
+							</div>
+
+							<div class="clear"></div>
+						</div>
+
+					</div>
+					<!--
+						Resume
+					-->
+					<div class="content resume">
+
+						<!-- title -->
+						<div class="title">Resume</div>
+
+						<!-- content -->
+						<div class="row">
+
+
+
+							<!-- experience -->
+							<div class="col col-d-12 col-t-12 col-m-12 border-line-v">
+								<div class="resume-title border-line-h">
+									<div class="icon"><i class="ion ion-briefcase"></i></div>
+									<div class="name">Experience</div>
+								</div>
+								<div class="resume-items">
+									<div class="resume-item border-line-h active">
+										<div class="date">2021 - Present</div>
+										<div class="name">Android Developer</div>
+										<div class="company">Freelance</div>
+										<p>
+											Implemented projects of medium complexity, adhering to the principles of Solid and MVVM/MVP
+											architecture. Working with open API services, receiving and processing data in the application.
+											Working with databases, user authorization, access to data on the phone (files, photos, bluetooth,
+											etc.). Development with recyclerVIew / WebView.
+										</p>
+									</div>
+								</div>
+							</div>
+
+
+							<!-- education -->
+							<div class="col col-d-12 col-t-12 col-m-12 border-line-v">
+								<div class="resume-title border-line-h">
+									<div class="icon"><i class="ion ion-university"></i></div>
+									<div class="name">Education</div>
+								</div>
+								<div class="resume-items">
+									<div class="resume-item border-line-h">
+										<div class="name">Android Basics in Kotlin</div>
+										<div class="company">developer.android.com</div>
+									</div>
+									<div class="resume-item border-line-h">
+										<div class="name">Kotlin for Beginner</div>
+										<div class="company">neco-desarrollo.es</div>
+									</div>
+									<div class="resume-item">
+										<div class="name">Kotlin. Fast Start / Android professional level </div>
+										<div class="company">Andrey Sumin - udemy.com</div>
+									</div>
+									<div class="resume-item">
+										<div class="name">Application development for Android</div>
+										<div class="company">Boris PRO IT</div>
+									</div>
+									<div class="resume-item">
+										<div class="name">Android Studio & Firebase</div>
+										<div class="company">Sergey Dragunov - udemy.com</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="clear"></div>
+						</div>
+
+					</div>
+
+
+				</div>
+			</div>
+
+			<!--
+				Card - Contacts
+			-->
+			<div class="card-inner contacts" id="contacts-card">
+				<div class="card-wrap">
+
+					<!--
+						Contact Form
+					-->
+					<div class="content contacts">
+
+						<!-- title -->
+						<div class="title">Contact Form</div>
+
+						<!-- content -->
+						<div class="row">
+
+
+							<div class="col col-d-12 col-t-12 col-m-12 border-line-v">
+								<div class="contact_form">
+
+									<p>
+										You can contact me through <a target="_blank" href="https://t.me/amezium/"><strong>social networks</strong></a> or by
+										writing to the mail using the form below.
+									</p>
+
+									<form id="cform" method="post">
+										<div class="row">
+											<div class="col col-d-6 col-t-6 col-m-12">
+												<div class="group-val">
+													<input type="text" name="name" placeholder="Full Name" />
+												</div>
+											</div>
+											<div class="col col-d-6 col-t-6 col-m-12">
+												<div class="group-val">
+													<input type="text" name="email" placeholder="Email Address" />
+												</div>
+											</div>
+											<div class="col col-d-12 col-t-12 col-m-12">
+												<div class="group-val">
+													<textarea name="message" placeholder="Your Message"></textarea>
+												</div>
+											</div>
+										</div>
+										<div class="align-left">
+											<a href="#" class="button" onclick="$('#cform').submit(); return false;">
+												<span class="text">Send Message</span>
+												<span class="arrow"></span>
+											</a>
+										</div>
+									</form>
+									<div class="alert-success">
+										<p>Thanks, your message is sent successfully.</p>
+									</div>
+								</div>
+							</div>
+							<div class="clear"></div>
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!--
+		jQuery Scripts
+	-->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.validate.js"></script>
+	<script src="js/jquery.magnific-popup.js"></script>
+	<script src="js/imagesloaded.pkgd.js"></script>
+	<script src="js/isotope.pkgd.js"></script>
+	<script src="js/jquery.slimscroll.js"></script>
+	<script src="js/owl.carousel.js"></script>
+
+	<!--
+		Google map api
+	-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDz2w7HUaWudHwd7AWQpCL48Qs050WOn9s"></script>
+
+	<!--
+		Main Scripts
+	-->
+	<script src="js/scripts.js"></script>
+
+</body>
+
+</html>
---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Amezium'"; //letter subject
+	$to = 'ameziumwork@gmail.com'; // Enter Your E-mail
+	$from = 'admin@amezium.dev'; // Admin e-mail
+	$fromName = 'Amezium'; // Your company name
+	$charset = 'UTF-8';
+?>
